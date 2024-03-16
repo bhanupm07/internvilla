3e770f807c305d4ef37d33ebb6938bab9316aaea
bhanu <bhanu2003@gmail.com>
2024-03-17 01:06:31 +0530

    cors issue

--- server/app.js
+++ server/app.js
@@ -4,6 +4,12 @@ const app = express();
 app.use(express.json());
 
 // app.use(cors());
+app.options("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.sendStatus(204);
+});
 
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
