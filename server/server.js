29eaf516ea0f24a92f08a56ba40f6aacd68029e5
bhanu <bhanu2003@gmail.com>
2024-03-17 00:20:55 +0530

    resolve: cors issue

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
-// const cors = require("cors");
+const cors = require("cors");
 dotenv.config({});
 
-// app.use(cors());
+app.use(
+  cors({
+    origin: "https://internvilla.vercel.app",
+  })
+);
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI)
