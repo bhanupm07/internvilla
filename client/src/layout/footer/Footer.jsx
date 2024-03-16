ab28522c5c512e8e55effd12eee02a83b342d266
bhanu <bhanu2003@gmail.com>
2024-03-16 23:33:02 +0530

    init: initializing the repo

---
+++ client/src/layout/footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-[#181326] text-sm text-gray-300 p-10 text-center mt-auto">
+      <span>Assignment built by </span>
+      <a href="https://www.linkedin.com/in/bhanupm/" className="text-blue-400">
+        @Bhanu Prakash Mahant
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
