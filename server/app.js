c6e8c9c9a30c0b69f1b4a7bf338c967c67400e48
bhanu <bhanu2003@gmail.com>
2024-03-17 18:05:37 +0530

    resolve: cors issue

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ app.use(helmet());
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://internvilla.vercel.app/"
+    "https://internvilla.vercel.app"
   );
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
