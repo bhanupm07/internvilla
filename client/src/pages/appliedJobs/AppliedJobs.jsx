abaf7294562b3a052d3a87a054267c27a0fd65fb
bhanu <bhanu2003@gmail.com>
2024-03-18 22:04:10 +0530

    add: loadersin appliedJobs page

--- client/src/pages/appliedJobs/AppliedJobs.jsx
+++ client/src/pages/appliedJobs/AppliedJobs.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import JobCard from "../../components/jobCard/JobCard";
 import { serverUrl } from "../../utils/constant";
+import { Spinner } from "@chakra-ui/react";
 
 const AppliedJobs = () => {
   const [appliedJobs, setAppliedJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const fetchAppliedJobs = async () => {
+    setIsLoading(true);
     const json = await fetch(`${serverUrl}/api/v1/jobs/applied`, {
       method: "GET",
       headers: {
@@ -15,6 +18,7 @@ const AppliedJobs = () => {
     });
     const data = await json.json();
     setAppliedJobs(data.appliedJobs);
+    setIsLoading(false);
   };
 
   useEffect(() => {
@@ -26,11 +30,21 @@ const AppliedJobs = () => {
       <h1 className="text-3xl text-white text-center my-2 font-bold">
         Your Applied Jobs
       </h1>
-      <main className="w-[90%] max-w-[500px] flex flex-col gap-5 items-center m-auto py-5">
-        {appliedJobs?.map((appliedJob) => (
-          <JobCard key={appliedJob._id} data={appliedJob} isJobApplied={true} />
-        ))}
-      </main>
+      {isLoading ? (
+        <Spinner size="xl" />
+      ) : (
+        <main className="w-[90%] max-w-[500px] flex flex-col gap-5 items-center m-auto py-5">
+          {appliedJobs.length > 0
+            ? appliedJobs?.map((appliedJob) => (
+                <JobCard
+                  key={appliedJob._id}
+                  data={appliedJob}
+                  isJobApplied={true}
+                />
+              ))
+            : "You haven't applied for any internships. Start applying now."}
+        </main>
+      )}
     </>
   );
 };
